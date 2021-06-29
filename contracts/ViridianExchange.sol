0e1d085629942735b0d5624d4fe88e0fca5008c8
Blake Hatch <blakewihatch@gmail.com>
2021-06-28 22:38:54 -0400

    Created token and nft, started exchange contract

---
+++ contracts/ViridianExchange.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./ViridianNFT.sol";
+
+contract ViridianExchange {
+
+    struct Trade {
+        uint[] nftIds;
+        uint vextAmt;
+        address _to;
+        address _from;
+    }
+
+    ViridianNFT[] public nfts;
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 var ViridianToken = artifacts.require("ViridianToken");
+var ViridianNFT = artifacts.require("ViridianNFT");
+
 
 module.exports = function(deployer) {
   deployer.deploy(ViridianToken);
+  deployer.deploy(ViridianNFT);
 };
