dd83c6c7c174875c7edb4b7b5d026c0e5df094c1
krishmoran <43583683+krishmoran@users.noreply.github.com>
2021-07-27 21:25:19 -0400

    New token impl, and added SafeMath

--- truffle-config.js
+++ truffle-config.js
@@ -44,8 +44,8 @@ module.exports = {
     // options below to some value.
     //
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 9545,            // Standard Ethereum port (default: none)
+     host: "192.168.1.151",     // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
      network_id: "5777",       // Any network (default: none)
     },
     // Another network with more advanced options...
