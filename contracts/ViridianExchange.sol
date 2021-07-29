a8f42b2e5398078734a42ac3f7c545404f8d9114
Blake Hatch <blakewihatch@gmail.com>
2021-07-29 17:03:13 -0400

    Minor changes

--- contracts/ViridianExchange.sol
+++ contracts/ViridianExchange.sol
@@ -4,6 +4,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 import "./ViridianNFT.sol";
