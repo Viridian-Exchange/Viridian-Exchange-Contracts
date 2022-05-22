e94498bc7987cbefa4f6d1ad643b0dc88b6d0b37
Blake Hatch <blakewihatch@gmail.com>
2022-05-22 15:27:34 -0400

    Removed misleading comment

--- contracts/ViridianExchange.sol
+++ contracts/ViridianExchange.sol
@@ -29,6 +29,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "erc721a/contracts/IERC721A.sol";
 import "@opengsn/contracts/src/BaseRelayRecipient.sol";
 
